906dc3e9bfd0ee679f3ee2ca24f5e17ba17b3bba
Alan Ktquez <ktquez@gmail.com>
2020-09-26 23:01:20 -0300

    Add initial config files

---
+++ build/config.js
@@ -0,0 +1,34 @@
+import path from 'path'
+import alias from '@rollup/plugin-alias'
+import PostCSS from 'rollup-plugin-postcss'
+import resolve from '@rollup/plugin-node-resolve'
+
+export default {
+  plugins: {
+    alias: [
+      alias({
+        entries: [
+          {
+            find: '@',
+            replacement: `${path.resolve(__dirname, '../src')}`
+          },
+        ],
+        customResolver: resolve({
+          extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue']
+        }),
+      })
+    ],
+    postcss: [
+      PostCSS({
+        modules: { generateScopedName: '[local]___[hash:base64:5]' },
+        include: /&module=.*\.css$/,
+      }),
+      PostCSS({ include: /(?<!&module=.*)\.css$/ })
+    ],
+    babel: {
+      exclude: 'node_modules/**',
+      extensions: ['.js', '.jsx', '.ts', '.tsx', '.vue'],
+      babelHelpers: 'bundled',
+    }
+  }
+}
---
+++ build/rollup.config.js
@@ -0,0 +1,49 @@
+import vue from 'rollup-plugin-vue'
+import json from '@rollup/plugin-json'
+import babel from '@rollup/plugin-babel'
+import replace from '@rollup/plugin-replace'
+import { terser } from 'rollup-plugin-terser'
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import peerDepsExternal from 'rollup-plugin-peer-deps-external'
+import defaultConfig from './config'
+
+export default commandLineArgs => {
+  return {
+    input: 'src/index.js',
+    plugins: [
+      replace({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      json(),
+      resolve(),
+      peerDepsExternal(),
+      ...defaultConfig.plugins.alias,
+      vue(),
+      ...defaultConfig.plugins.postcss,
+      babel({
+        ...defaultConfig.plugins.babel,
+        presets: [
+          [
+            '@babel/preset-env'
+          ]
+        ]
+      }),
+      commonjs(),
+      commandLineArgs.format === 'iife' && terser({
+        output: {
+          ecma: 5
+        }
+      })
+    ],
+    output: {
+      compact: true,
+      name: 'VueAxe',
+      exports: 'named',
+      globals: {
+        vue: 'Vue',
+        'axe-core': 'axeCore'
+      }
+    }
+  }
+}
