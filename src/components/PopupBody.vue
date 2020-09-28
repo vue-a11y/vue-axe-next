8bfc7d2755b5eb93e4798021d63f79a316c17e07
Alan Ktquez <ktquez@gmail.com>
2020-09-27 23:16:08 -0300

    Add loading and style subitem violation

--- src/components/PopupBody.vue
+++ src/components/PopupBody.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="va-popup__body" v-if="results.issuesFound">
+  <div
+    v-if="results.issuesFound"
+    class="va-popup__body"
+  >
+    <PopupLoading v-show="loading" />
     <div class="va-popup__body-info">
       <span>{{ results.issuesFound }} issue{{ results.issuesFound > 1 ? 's' : '' }} found</span>
       <span>{{ results.lastAudition }}</span>
     </div>
     <ul class="va-popup-list">
       <li
-        class="va-popup-item"
         v-for="(result, key) in results.impacts"
         :key="key"
+        class="va-popup-item"
       >
         <template v-if="result.length">
           <div class="va-popup-item__header">
@@ -16,31 +20,87 @@
             <span>{{ result.length }} issues</span>
           </div>
           <ul class="va-popup-sublist">
-            <li class="va-popup-subitem">
-              <!-- violations -->
-            </li>
+            <WrapperCard
+              v-for="(violation, index) in result"
+              :key="`subitem-${violation.impact}-${index}`"
+              tag="li"
+              class="va-popup-subitem"
+              @trigger="goViolationDetails"
+            >
+              <div class="va-flex">
+                <span :class="`va-popup-subitem__disc va-popup-subitem__disc--${violation.impact}`">●</span>
+                <span :id="`violation-${violation.id}`">
+                  {{ violation.description }}
+                </span>
+              </div>
+              <button
+                type="button"
+                class="va-default-btn va-popup-subitem__button"
+                :aria-labelledby="`see-more-${violation.id} violation-${violation.id}`"
+                @click="goViolationDetails"
+              >
+                <span
+                  :id="`see-more-${violation.id}`"
+                  class="va-sr-only"
+                >See more</span>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="13.2"
+                  height="6.7"
+                  focusable="false"
+                  aria-hidden="true"
+                >
+                  <path
+                    d="M9.8.7l2.7 2.7m0 0L9.8 6m2.7-2.6H.5"
+                    fill="none"
+                    stroke="#000"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </button>
+            </WrapperCard>
           </ul>
         </template>
       </li>
     </ul>
   </div>
-  <div class="va-popup__body" v-if="!results.issuesFound">
+  <div
+    v-show="!results.issuesFound"
+    class="va-popup__body"
+  >
     WITHOUT ERRORS
   </div>
 </template>
 
 <script>
 import { inject } from 'vue'
-import { vueAxe } from '../utils/constants'
+import { vueAxe } from '@/utils/constants'
+
+import PopupLoading from '@/components/PopupLoading'
+import WrapperCard from '@/components/WrapperCard'
 
 export default {
   name: 'PopupBody',
 
+  disableAxeAudit: true,
+
+  components: {
+    WrapperCard,
+    PopupLoading
+  },
+
   setup () {
-    const { results } = inject(vueAxe)
+    const { loading, results } = inject(vueAxe)
+
+    function goViolationDetails () {
+      console.log('yes')
+    }
 
     return {
-      results
+      loading,
+      results,
+      goViolationDetails
     }
   }
 }
@@ -48,6 +108,8 @@ export default {
 
 <style scoped>
 .va-popup__body {
+  position: relative;
+  z-index: 1;
   padding: var(--padding);
   background-color: white;
   min-height: 300px;
@@ -115,4 +177,54 @@ export default {
 .va-popup-item__header--minor {
   color: var(--va-minor);
 }
+
+.va-popup-sublist {
+  margin-top: 8px;
+}
+
+.va-popup-subitem {
+  display: flex;
+  justify-content: space-between;
+  align-items: start;
+  padding: 14px 4px;
+  font-size: 1rem;
+}
+
+.va-popup-subitem:not(:last-child) {
+  border-bottom: 1px solid var(--va-border-color);
+}
+
+.va-popup-subitem:hover, .va-popup-subitem:focus-within {
+  background-color: var(--va-bg-item);
+  border-radius: 4px;
+}
+
+.va-popup-subitem__disc {
+  padding-left: 4px;
+  padding-right: 8px;
+}
+
+.va-popup-subitem__disc--critical {
+  color: var(--va-critical);
+}
+
+.va-popup-subitem__disc--serious {
+  color: var(--va-serious);
+}
+
+.va-popup-subitem__disc--moderate {
+  color: var(--va-moderate);
+}
+
+.va-popup-subitem__disc--minor {
+  color: var(--va-minor);
+}
+
+.va-popup-subitem__button {
+  position: relative;
+  top: -2px;
+  margin-left: 4px;
+  padding: 4px 10px;
+}
+
 </style>
