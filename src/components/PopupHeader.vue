475234e01e96c086de6e80212ce30afe610ceba7
Alan Ktquez <ktquez@gmail.com>
2020-09-30 22:22:29 -0300

    Update display version

--- src/components/PopupHeader.vue
+++ src/components/PopupHeader.vue
@@ -32,8 +32,8 @@
       </svg>
     </a>
     <div class="va-text-right va-text-sm va-font-medium">
-      <span>vue-axe: {{ versions.vueAxe }}</span> <br>
-      <span>axe-core: {{ versions.axeCore }}</span>
+      <span>{{ versions.vueAxe }} (vue-axe)</span> <br>
+      <span>{{ versions.axeCore }} (axe-core) </span>
     </div>
   </div>
 </template>
